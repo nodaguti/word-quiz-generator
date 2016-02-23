a53bfa6ce5d67aa6e087fd5063a6e1dc33202b56
nodaguti <nodaguti@gmail.com>
2016-02-24 00:39:55 +0900

    Stop exposing preprocessors, lemmatizers and utils

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import QuizGenerator from './quiz-generator.js';
 import Source from './source.js';
-import preprocessors from './preprocessor/index';
-import lemmatizers from './lemmatizer/index';
-import utils from './utils.js';
 
 export default QuizGenerator;
-
-export {
-  Source,
-  preprocessors,
-  lemmatizers,
-  utils,
-};
+export { Source };
