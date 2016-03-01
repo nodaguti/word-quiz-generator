25f7a05373c86eb75d07125e7ea2e4ee6495ba61
nodaguti <nodaguti@gmail.com>
2016-03-01 16:18:01 +0900

    Add the help message to 'generate' command

--- src/command/generate.js
+++ src/command/generate.js
@@ -3,6 +3,31 @@ import minimist from 'minimist';
 import colors from 'colors';
 import QuizGenerator from '../quiz-generator.js';
 
+const showUsage = () => {
+  console.log(
+`word-quiz-generator generate --help
+word-quiz-generator generate --material=<path> --sources=<paths> --sections --size [--instruction] [--sentenceSeparator=<RegExp>] [--clauseRegExp=<RegExp>] [--wordRegExp=<RegExp>] [--wordBoundaryRegExp=<RegExp>] [--abbrRegExp=<RegExp>]
+Generate a quiz and put it to stdout using the given material and sources.
+
+-h, --help: Show this usage.
+-m, --material=<path>: Path to a material.
+-s, --sources=<paths>: Comma-separated path strings to sources.
+-e, --sections=<sections>: Quiz coverage. e.g. '1-10', '5', '3-'
+-i, --size=<num>: The number of questions.
+--instruction: The instruction text located at top of the quiz. Default: 'Write down the meaning of underlined words/phrases.'
+--skip-spaces: Specify if a language you want to make a quiz has no word divider, such as Japanese and Chinese.
+
+The following options determines how to extract a word/phrase or sentence from a text.
+For English quiz, these are automatically set and usually don't need to override them.
+
+--sentenceSeparator=<RegExp>: Regular expression representing a sentence separator. Default: '(?:[?!.]\\s?)+"?(?:\\s|$)(?!,)'
+--clauseRegExp=<RegExp>: Regular expression representing a clause. Default: '[^,:"?!.]+'
+--wordRegExp=<RegExp>: Regular expression representing a word. Default: '[\\w'-]+'
+--wordBoundaryRegExp=<RegExp>: Regular expression representing a word boundary. Default: '\\b'
+--abbrRegExp=<RegExp>: Regular expression representing an abbreviation mark. Default: '\\.\\.\\.'
+`);
+};
+
 export default async function (args) {
   const argv = minimist(args, {
     string: [
@@ -19,19 +44,29 @@ export default async function (args) {
     ],
     boolean: [
       'skip-spaces',
+      'help',
     ],
     alias: {
       m: 'material',
-      t: 'texts',
-      l: 'lemmatized',
-      s: 'sections',
-      S: 'size',
+      s: 'sources',
+      e: 'sections',
+      i: 'size',
     },
     default: {
       instruction: 'Write down the meaning of underlined words/phrases.',
       wordRegExp: '[\\w\'-]+',  // need for display
     },
   });
+
+  if (argv.help) {
+    showUsage();
+    process.exit(0);
+  }
+
+  if (!argv.material || !argv.sources || !argv.sections || !argv.size) {
+    showUsage();
+    process.exit(1);
+  }
 
   const material = argv.material;
   const sources = argv.sources;
